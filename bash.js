7a2350391459d3184fe0f5471bb66147b995af09
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-06-12 12:57:50 -0400

    pwd module created

---
+++ bash.js
@@ -0,0 +1,10 @@
+const checkPwd = require('./pwd');
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    checkPwd();
+  }
+});
---
+++ pwd.js
@@ -0,0 +1,6 @@
+var checkPwd = function () {
+    process.stdout.write(__dirname);
+    process.stdout.write('\nprompt > ');
+}
+
+module.exports = checkPwd;
